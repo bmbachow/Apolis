e22f987fbfb585714d0c72ff82a0cc29e0b5778e
Brian Bachow <bmbachow@gmail.com>
2022-03-07 07:17:35 -0800

    finished assignment next push try

---
+++ swiftUIAssignment1/swiftUIAssignment1/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  swiftUIAssignment1
+//
+//  Created by Brian Bachow on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            HStack{
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+                
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+                Text("SwiftUI").foregroundColor(.red)
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIAssignment1/swiftUIAssignment1/swiftUIAssignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIAssignment1App.swift
+//  swiftUIAssignment1
+//
+//  Created by Brian Bachow on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIAssignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
